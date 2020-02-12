094916027bb4374ac666966dd3cb56d15b95b745
Ho Si Binh <hsbinh@live.com>
2020-02-13 00:37:55 +0800

    Add backend task validation

--- app/models/task.rb
+++ app/models/task.rb
@@ -8,4 +8,12 @@ class Task < ApplicationRecord
     belongs_to :user
     default_scope -> { order(created_at: :desc) }
     has_and_belongs_to_many :tags
+    
+    VALID_DEADLINE_REGEXP = /[0-3]{1}[0-9]{1}\/[0-1]{1}[0-9]{1}\/[1-2]{1}[0-9]{3}/
+
+    validates :deadline, format: { with: VALID_DEADLINE_REGEXP,
+      message: "wrong date format" }
+    validates :name, :category, presence: true
+
+    # TO DO: Handle duplicate tasks
 end
